451241decff4473fd6e571dc46a188ef5a2f109d
Brian Bansenauer <brianb@cascadia.edu>
2015-05-03 22:41:11 -0700

    initial commit

---
+++ HelloWorld-Swift/HelloWorld-Swift/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  HelloWorld-Swift
+//
+//  Created by Brian Bansenauer on 5/3/15.
+//  Copyright (c) 2015 Cascadia College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var lblOutput: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func showOutput(sender: AnyObject) {
+        lblOutput.text = "Hello " + txtName.text
+        //lblOutput.text = "Hello \(txtName.text)"
+    }
+    
+    @IBAction func backgroundTap(sender: AnyObject){
+        self.view.endEditing(true);
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
